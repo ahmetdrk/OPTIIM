dd7a8f4b50dd784d836dd5ebfd232f9b495df432
Ahmet Doruk <97681607+ahmetdrk@users.noreply.github.com>
2022-11-18 03:53:23 +0300

    Update TestBase.java

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,2 +1,40 @@
-package tests;public class TestBase {
+package tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import utilities.WebDriverFactory;
+
+import java.util.concurrent.TimeUnit;
+
+public abstract class TestBase {
+
+    public WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--incognito");
+        options.addArguments("--start-maximized");
+        options.addArguments("--ignore-certificate-errors");
+        options.addArguments("--allow-insecure-localhost");
+        options.addArguments("--acceptInsecureCerts");
+        options.addArguments("--disable-blink-features=AutomationControlled");
+        options.addArguments("--disable-extensions");
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver(options);
+
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.close();
+    }
 }
